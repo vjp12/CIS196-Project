e649b3e69966e15425e2b719d0b6e9f7935c623f
vjp12 <vijayprabakaran12@gmail.com>
2014-03-24 23:59:56 -0400

    hw8- heroku update

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require File.expand_path('../application', __FILE__)
 Homework1::Application.initialize!
 
 #mail
-ActionMailer::Base.smtp_settings = {
-	:user_name => 'vjp12',
-	:password => 'Sendmeaway57',
-	:domain => 'http://secret-fjord-1158.herokuapp.com/',	
-	:port => 587,
-	:authentication => :plain,
-	:enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+# 	:user_name => 'vjp12',
+# 	:password => 'Sendmeaway57',
+# 	:domain => 'http://secret-fjord-1158.herokuapp.com/',	
+# 	:port => 587,
+# 	:authentication => :plain,
+# 	:enable_starttls_auto => true
+# }
