ca2cd8c7ff52512967dd30e1c0f87abb7cc97b0c
vjp12 <vijayprabakaran12@gmail.com>
2014-03-24 23:54:17 -0400

    hw8- heroku update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Homework1::Application.configure do
  config.action_mailer.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
+    :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => ENV['SENDGRID_DOMAIN']
