bad70d06a684b52472254ef31309d7ca4e5baeb9
vjp12 <vijayprabakaran12@gmail.com>
2014-05-09 04:57:41 -0400

    added toggle buttons to stock show

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,21 +1,27 @@
 $(function(){
 	$(".dash-review").addClass("hide");
 	$(".dash-review2").addClass("hide");
+	$(".dash-review3").addClass("hide");
+	$(".dash-review4").addClass("hide");
 	$(".hide-btn").click(function(){
 		if($(".dash-review").hasClass("hide")){
 			$(".dash-review").removeClass("hide");
+			$(".dash-review3").removeClass("hide");
 			$(".hide-btn").text("Hide my investments");
 		} else {
 			$(".dash-review").addClass("hide");
+			$(".dash-review3").addClass("hide");
 			$(".hide-btn").text("Show my investments");
 		}
 	});
 	$(".hide-btn2").click(function(){	
 		if($(".dash-review2").hasClass("hide")){
 			$(".dash-review2").removeClass("hide");
+			$(".dash-review4").removeClass("hide");
 			$(".hide-btn2").text("Hide latest investments");
 		} else {
 			$(".dash-review2").addClass("hide");
+			$(".dash-review4").addClass("hide");
 			$(".hide-btn2").text("Show latest investments");
 		}
 	});
