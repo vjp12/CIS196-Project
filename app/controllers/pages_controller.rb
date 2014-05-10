0e6f083389cf3a1d06713a60e6650492c3f621f6
vjp12 <vijayprabakaran12@gmail.com>
2014-05-09 23:59:42 -0400

    values work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,20 @@ class PagesController < ApplicationController
   def index
     ##redirect_to refresh_stocks_path
 
-  	@members = Member.all.order(:value)
+    portfolios = Portfolio.all   
+	portfolios.each do |portfolio|
+	  value = BigDecimal("0")
+      investments = Investment.where(member_id: portfolio.member.id)
+      investments.each  do |investment|
+        stock_id = investment.stock_id
+        stock = Stock.where(stock_id).first
+        price = StockQuote::Stock.quote(stock.ticker).last_trade_price_only
+        value += BigDecimal(investment.share_change.to_s) * price
+      end
+      portfolio.market_value = value.to_s
+      portfolio.save
+    end
+  	@members = portfolios.order('market_value DESC')
   	@i = 1
   end
 end
